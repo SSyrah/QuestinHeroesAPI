8ab0dae458b1eb678dc6d575f8eacdc86486d9a3
J-M <zzuhis@gmail.com>
2022-01-31 10:12:51 +0200

    GameController

---
+++ src/main/java/com/example/Heroes/GameController.java
@@ -0,0 +1,19 @@
+package com.example.Heroes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GameController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index";
+    }
+
+    @GetMapping("*")
+    public String getDefault(){
+        return "redirect:/";
+    }
+
+}
